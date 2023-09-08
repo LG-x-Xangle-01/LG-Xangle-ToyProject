e4439320d4cb6f5e9950104064bfe58205336d5c
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-09-09 03:47:56 +0900

    Fix: python errors

--- components/FileConvert.js
+++ components/FileConvert.js
@@ -72,29 +72,3 @@ const addBlueMark = async (imageURL) => {
     }
   });
 };
-
-// // 블루마크 추가 함수
-// const addBlueMark = async (imageURL) => {
-//   const img = await loadImage(imageURL);
-
-//   // Canvas 생성
-//   const canvas = document.createElement("canvas");
-//   canvas.width = img.width;
-//   canvas.height = img.height;
-//   const ctx = canvas.getContext("2d");
-
-//   // 원본 이미지 그리기
-//   ctx.drawImage(img, 0, 0);
-
-//   // 블루마크(여기서는 텍스트) 추가
-//   ctx.fillStyle = "blue";
-//   ctx.font = "48px serif";
-//   ctx.fillText("LG", 50, 50);
-
-//   // Canvas에서 Blob URL 생성
-//   canvas.toBlob((blob) => {
-//     const newImageURL = URL.createObjectURL(blob);
-//     // newImageURL을 사용하여 이미지 표시 또는 다른 작업 수행
-//     console.log(newImageURL);
-//   });
-// };
